f3fcdf3b571b6a2bccba7aa9da50865795f9a16c
Vinay Pant <vinaypant24@gmail.com>
2025-02-03 00:18:13 +0000

    Initiating Dashboard Control

---
+++ ttkbootstrap/home_class.py
@@ -0,0 +1,55 @@
+import ttkbootstrap as btk
+import time
+from threading import Thread
+
+
+
+
+
+
+class Dashboard():
+
+    def __init__(self , master ) -> None:
+
+
+        ### Static variables for the controls  :
+        self.username_var = btk.StringVar(value="Enter Username")
+        self.password_var = btk.StringVar(value="Enter Password")
+        self.checkvar  = btk.BooleanVar(value=False)
+        self.page_depth_variable_initial  = btk.IntVar(value=0)
+        self.page_depth_variable_final  = btk.IntVar(value = 3)
+        self.retries_variable  = btk.IntVar(value  = 10)
+        self.keywords_var  = btk.StringVar(value="Enter Keywords")
+        
+
+        ## Controls for the dashboard :
+        self.username_box  = btk.Entry(master = master  , textvariable=self.username_var , width=31 )
+
+
+        
+
+
+
+        # Configuring Controls  : 
+        # self.username_box.insert(0, self.username_var.get())
+        self.username_box.insert(0, "Enter Username and the passwrod")
+
+
+
+
+
+
+
+
+        ## Packing Controls : 
+        self.username_box.pack()
+
+
+
+
+
+if __name__ == "__main__":
+    main_window  = btk.Window()
+    Dashboard(master = main_window)
+
+    main_window.mainloop()
--- ttkbootstrap/views.py
+++ ttkbootstrap/views.py
@@ -12,6 +12,12 @@ import ctypes
 
 from PIL import Image , ImageTk
 
+
+
+
+### Importing classes for the controls : 
+import home_class
+
 ## Setting up current directory in which the script would run :
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
@@ -32,8 +38,6 @@ settings_image  = Image.open("settings.png")
 connections = connections_image.resize((30 , 30))
 jobs  = jobs_image.resize((30 , 30))
 settings = settings_image.resize((30 , 30))
-
-
 
 
 
@@ -85,12 +89,41 @@ def change_window(window_name):
         second_screen.pack_forget()
         third_screen.pack_forget()
 
-
+def checkbuttonclicked():
+        if checkvar.get() == True:
+            password_entry.configure(show="")
+           
+        elif checkvar.get() == False:
+            if password_entry.get() == "Enter Password":
+                pass
+            else:
+                password_entry.configure(show="*")
         
+def check_button_check(x):
+        if checkvar.get() == True:
+            password_entry.configure(show="")
+           
+        elif checkvar.get() == False:
+            if password_entry.get() == "Enter Password":
+                pass
+            else:
+                password_entry.configure(show="*")
+
 
 
 ## Main Window and Skeleton details : 
 window  = btk.Window(themename=themename)
+
+
+
+### Variables for the windows : 
+username_var = btk.StringVar(value="Enter Username")
+password_var = btk.StringVar(value="Enter Password")
+checkvar  = btk.BooleanVar(value=False)
+page_depth_variable_initial  = btk.IntVar(value=0)
+page_depth_variable_final  = btk.IntVar(value = 3)
+retries_variable  = btk.IntVar(value  = 10)
+keywords_var  = btk.StringVar(value="Enter Keywords")
 
 connections_icon = ImageTk.PhotoImage(connections)
 jobs_icon  = ImageTk.PhotoImage(jobs)
@@ -129,6 +162,16 @@ home_button_with_image  = btk.Button(home_button_frame  , image=connections_icon
 jobs_button_with_image  = btk.Button(jobs_button_frame , image=jobs_icon , compound="top" , bootstyle  = success_theme , command= lambda : change_window("jobs_button"))
 settings_button_with_image  = btk.Button(settings_button_frame , image=settings_icon , compound="top" , bootstyle  = success_theme , command= lambda : change_window("settings_button"))
 
+### Frame wise controls : 
+username_entry  = btk.Entry(master=first_screen , textvariable=username_var , width=41)
+password_entry  = btk.Entry(master = first_screen , textvariable=password_var , width=41)
+showpasswordcheckbox = btk.Checkbutton(master = first_screen , text="Show Password" , variable=checkvar , command=checkbuttonclicked  ,bootstyle="square-toggle")
+
+
+
+
+
+
 ## Configuring Controls :
 sidebar.pack_propagate(0)
 first_screen.pack_propagate(0)
@@ -137,6 +180,8 @@ third_screen.pack_propagate(0)
 home_button_frame.pack_propagate(0)
 jobs_button_frame.pack_propagate(0)
 settings_button_frame.pack_propagate(0)
+
+
 
 
 ## Binding Functions : 
@@ -150,6 +195,10 @@ def sidebar_animation(expand , width):
         sidebar.configure(width=width)
         window.after(30 , lambda : sidebar_animation(False , width))
 
+def clear_password_box():
+        password_entry.delete( 0 ,tk.END)
+        sleep(1 / 4)
+        password_entry.configure(show="*")
 
 ### Binding Controls  : 
 sidebar.bind("<Enter>" , lambda x:sidebar_animation(expand=True , width=50))
@@ -157,6 +206,10 @@ sidebar.bind("<Leave>" , lambda x : sidebar_animation(expand=False , width=100))
 home_button_frame.bind("<Enter>" , lambda x : sidebar_animation(expand=True , width=100))
 jobs_button_frame.bind("<Enter>" , lambda x : sidebar_animation(expand=True , width=100))
 settings_button_frame.bind("<Enter>" , lambda x : sidebar_animation(expand=True , width=100))
+
+username_entry.bind("<FocusIn>" , lambda x : username_entry.delete(0 , 190))
+password_entry.bind("<FocusIn>" , lambda x : clear_password_box())
+
 
 ## Tooltip for the widgets  : 
 # connections_button_tooltip = ToolTip(home_button_with_image , text="Click for Connections Page" , bootstyle="danger")
@@ -168,6 +221,9 @@ first_screen.pack(side=tk.LEFT ,expand=True , fill=tk.X)
 # second_screen.pack(side=tk.LEFT ,expand=True , fill=tk.X)
 # third_screen.pack(side=tk.LEFT , expand=True , fill = tk.X)
 
+
+
+
 home_button_frame.pack(side=tk.TOP , expand=True , fill=tk.X)
 jobs_button_frame.pack(side=tk.TOP , expand=True , fill=tk.X)
 settings_button_frame.pack(side=tk.TOP , expand=True ,fill=tk.X)
@@ -175,5 +231,16 @@ settings_button_frame.pack(side=tk.TOP , expand=True ,fill=tk.X)
 home_button_with_image.pack(expand=True , fill=tk.BOTH)
 jobs_button_with_image.pack(expand=True , fill=tk.BOTH)
 settings_button_with_image.pack(expand=True , fill=tk.BOTH)
+
+
+
+
+
+
+##### Packing Frame wise controls : 
+username_entry.pack (side=tk.LEFT , anchor = tk.N , padx = 10 , pady=10)
+password_entry.pack(side=tk.LEFT , anchor=tk.N , pady=10 , padx= 10)
+showpasswordcheckbox.pack(side=tk.RIGHT , anchor=tk.N , pady=19 , padx=(0,30))
+
 
 window.mainloop()
